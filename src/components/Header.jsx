1c79e4f655fde117fba93aa239ed1b8071935683
mussameeks <mussameeks@gmail.com>
2025-05-28 20:19:08 +0200

    Add files via upload

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,9 @@
-
 import React from 'react';
-import { Link } from 'react-router-dom';
-import logo from '../assets/logo.png';
 
-const Header = () => {
-  return (
-    <header className="bg-blue-900 text-white p-4 shadow">
-      <div className="container mx-auto flex justify-between items-center">
-        <Link to="/" className="text-lg font-bold flex items-center space-x-2">
-          <img src={logo} alt="TwijiStats" className="w-8 h-8" />
-          <span>TwijiStats</span>
-        </Link>
-      </div>
-    </header>
-  );
-};
+const Header = () => (
+  <header className="bg-gray-900 text-white p-4 text-center text-xl font-bold">
+    TwijiStats Dashboard
+  </header>
+);
 
 export default Header;
